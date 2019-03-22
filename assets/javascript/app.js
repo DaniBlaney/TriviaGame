0cb5484b899582a34996ab089b2843361311d528
Danielle Blaney <daniblaney@gmail.com>
2019-03-21 21:33:05 -0400

    fixed shake on flipped img

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -221,7 +221,55 @@ function scoreboard(){
 	$('#startOverBtn').show();
 	$('#startOverBtn').html('Start Over?');
 }
+// //animate daleks
+// $(document).ready(function() {
+//   animateDiv($('.a'));
+//       animateDiv($('.b'));
+//       animateDiv($('.c'));
 
+// });
+
+// function makeNewPosition(image) {
+
+//   // Get viewport dimensions (remove the dimension of the div)
+//   var h = image.height() - 50;
+//   var w = image.width() - 50;
+
+//   var nh = Math.floor(Math.random() * h);
+//   var nw = Math.floor(Math.random() * w);
+
+//   return [nh, nw];
+
+// }
+
+// function animateDiv($target) {
+//   var newq = makeNewPosition($target.parent());
+//   var oldq = $target.offset();
+//   var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+//   $target.animate({
+//       top: newq[0],
+//       left: newq[1]
+//   }, speed, function() {
+//       animateDiv($target);
+//   });
+
+// };
+
+// function calcSpeed(prev, next) {
+
+//   var x = Math.abs(prev[1] - next[1]);
+//   var y = Math.abs(prev[0] - next[0]);
+
+//   var greatest = x > y ? x : y;
+
+//   var speedModifier = 0.1;
+
+//   var speed = Math.ceil(greatest / speedModifier);
+
+//   return speed;
+
+// }​
 // function PlaySound(mySound) {
 //   var thissound=document.getElementById(mySound);
 //   thissound.play();

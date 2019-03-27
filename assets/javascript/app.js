e58901f5fed8613133a08e7755fcd80109f6c42f
Danielle Blaney <daniblaney@gmail.com>
2019-03-26 21:23:58 -0400

    fixed audio onload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -120,6 +120,17 @@ $('#startOverBtn').on('click', function(){
 	$(this).hide();
 	newGame();
 });
+$(document).ready()
+
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if(!isChrome){
+    $('#iframeAudio').remove()
+  }
+  else{
+  $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background
+  };
+
+
 //creates new game
 function newGame(){
 	$('#finalMessage').empty();
